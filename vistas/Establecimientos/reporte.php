a261d06cc8da9a55133a05ecc74c7da7bccb4997
CesarMon16 <gandhineitor@gmail.com>
2025-07-15 20:41:49 -0600

    Add files via upload

--- vistas/Establecimientos/reporte.php
+++ vistas/Establecimientos/reporte.php
@@ -1,33 +1,36 @@
 <?php 
 ob_start();
-include('../../head.php');
+
 include('../../modelos/Establecimiento.php'); 
 
 $establecimiento = Establecimiento::lista();
 ?>
 <html>
- <table class="table table-responsive table-striped" id="tabla-catalogo">
+    <?php include('../../head.php') ?>
+    <h1>Reporte de establecimientos</h1>
+ <table class="table table-responsive table-striped " border="1"  id="tabla-catalogo">
                 <tr>
                     <th>Id</th>
                     <th>Nombre</th>
                     <th>Descripcion</th>
                     <th>Horario</th>
                     <th>Dias laborales</th>
-                    <th>Foto</th>
                     <th>Direccion</th>
                   
                     
                 </tr>
+                <
                 <?php 
                 foreach($establecimiento as $e){
                 ?>
+                <br>
                  <tr>
                     <td> <?php echo $e->Id_Establecimiento; ?> </td>
                     <td> <?php echo $e->Nombre; ?></td>
                     <td> <?php echo $e->Descripcion; ?></td>
                     <td> <?php echo $e->Horario; ?></td>
                     <td> <?php echo $e->Dias_labo; ?></td>
-                    <td><img src="/../../http://<?php echo $_SERVER['HTTP_HOST'];?>/servifinder/<?php echo $e->Foto; ?>" alt="Foto de <?php echo $e->Nombre; ?>" width="100rem"></td>
+                    
                     <td> <?php echo $e->DireccionyRef; ?></td>
                    
 
@@ -55,7 +58,7 @@ $dompdf-> setPaper('letter');
 
 $dompdf->render();
 
-$dompdf->stream("reporte_Establecimientos.pdf", array("Attachment" => false));
+$dompdf->stream("reporte_Establecimientos.pdf", array("Attachment" => true));
 
 
 
