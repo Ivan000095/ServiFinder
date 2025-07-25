731ffaa7a80221176fe5a1c838fee1e64afd0ddf
Ivan000095 <ivanlanestosa8@gmail.com>
2025-07-24 20:55:02 -0600

    Delete vistas directory

--- vistas/Doctores/destroy.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('../../modelos/Doctor.php');
-    $id = $_REQUEST['idDoctor'];
-    $Doctor = Doctor::find($id);
-    $Doctor->destroy();
-    echo '<meta http-equiv="refresh" content="0;url=/Integradora/vistas/Doctores">';
-?>
--- vistas/Establecimientos/destroy.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require('../../modelos/Establecimiento.php');
-    $Id_Establecimiento = $_REQUEST['Id_Establecimiento'];
-    $establecimiento = Establecimiento::find($Id_Establecimiento);
-    $establecimiento->destroy();
-    echo '<meta http-equiv="refresh" content="0;url=index.php">';
-?>
